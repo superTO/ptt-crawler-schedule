7f62160e2fb5f1f9e01369e6e5fc7f367d7d1a52
superTO <to840116@gmail.com>
2025-10-20 20:38:47 +0800

    Feat: update data.js

--- data.js
+++ data.js
@@ -13,7 +13,7 @@ export const searchOption = [
 		lineTitle: '股板大神發文啦!',
 		pages: 3,
 		option : {
-			authors: ['f204137', 'yinglinga', 'zesonpso', 'book1999', 'onekoni', 'leo15824', 'Crypto', 'mrp']
+			authors: ['f204137', 'yinglinga', 'zesonpso', 'book1999', 'onekoni', 'leo15824', 'Crypto', 'mrp', 'MrChen']
 		}
 	},
 	{
